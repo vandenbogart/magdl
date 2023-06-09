8187c42dc9ad0e7585f6e7ffe6bfaa7a1ec2cd3a
Eric Bogard <eric.bogard@bitsighttech.com>
2023-06-09 16:48:21 -0700

    Connect to trackers

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,30 @@
 //! Minimal bittorrent download client
 //!
 
+use std::{net::{self, ToSocketAddrs, UdpSocket}, time::Duration};
+
+use anyhow::{Context, bail};
+use byteorder::{BigEndian, ByteOrder};
+use rand::random;
 
 
-struct Magdl;
+
+pub struct Magdl;
 impl Magdl {
-    pub fn download(magnet_link: String) {
+    pub fn download(magnet_link: String) -> anyhow::Result<()> {
         // Parse magnet link into trackers and info hash
-        let magnet = Magnet::from(magnet_link)
+        let magnet = Magnet::from_link_string(magnet_link);
 
         // Initialize trackers and construct list of available peers
-        todo!();
+        let tracker_connections: Vec<TrackerConnection> = magnet.tracker_urls.into_iter().filter_map(|url| {
+            match TrackerConnection::establish(url) {
+                Ok(conn) => {
+                    println!("Connected to: {}", conn.url);
+                    Some(conn)
+                },
+                Err(e) => { println!("{}", e); None },
+            }
+        }).collect();
 
         // Connect to as many peers as possible and perform handshake
         todo!();
@@ -33,8 +47,8 @@ struct Magnet {
     info_hash: [u8; 20],
     display_name: String,
 }
-impl From<String> for Magnet {
-    fn from(value: String) -> Self {
+impl Magnet {
+    fn from_link_string(value: String) -> Self {
         let decoded = urlencoding::decode(&value).expect("Failed to parse magnet link");
         let slice = &decoded[8..];
         let split = slice.split("&").collect::<Vec<_>>();
@@ -69,3 +83,48 @@ impl From<String> for Magnet {
         }
     }
 }
+
+struct TrackerConnection {
+    socket: UdpSocket,
+    pub connection_id: i64,
+    pub url: url::Url,
+}
+impl TrackerConnection {
+    pub fn establish(url: url::Url) -> anyhow::Result<Self> {
+        let host_port = format!("{}:{}", url.host_str().unwrap(), url.port().unwrap_or(80));
+        let socket_addr = host_port.to_socket_addrs()?.next().unwrap();
+
+        // maybe only use one socket in the future
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        socket.set_read_timeout(Some(Duration::from_secs(3)))?;
+        UdpSocket::connect(&socket, socket_addr)?;
+
+        const PROTOCOL_ID: i64 = 0x41727101980;
+        let mut connection_request = [0u8; 16];
+        let transaction_id = random::<u32>();
+        BigEndian::write_i64(&mut connection_request[0..8], PROTOCOL_ID);
+        BigEndian::write_u32(&mut connection_request[8..12], 0);
+        BigEndian::write_u32(&mut connection_request[12..16], transaction_id);
+        socket.send(&connection_request)?;
+
+        let mut connection_response = [0u8; 16];
+
+        let bytes_read = socket.recv(&mut connection_response)?;
+        if bytes_read != 16 {
+            bail!("Invalid number of bytes read from tracker");
+        }
+        let response_transaction_id = BigEndian::read_u32(&connection_response[4..8]);
+        let connection_id = BigEndian::read_i64(&connection_response[8..16]);
+        if transaction_id != response_transaction_id {
+            bail!("Mismatched transaction ID returned from tracker");
+        }
+        Ok(Self {
+            socket,
+            connection_id,
+            url,
+        })
+    }
+}
+
+
+
