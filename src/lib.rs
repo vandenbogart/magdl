ab6b2cc16b72caf160065f0a8dfe35844f13bc3a
Eric Bogard <eric.bogard@bitsighttech.com>
2023-06-09 18:55:49 -0700

    Basic framework to process peer connections

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,20 @@
 //! Minimal bittorrent download client
 //!
 
-use std::{net::{self, ToSocketAddrs, UdpSocket}, time::Duration};
+use std::{
+    net::{self, IpAddr, Ipv4Addr, SocketAddr, TcpStream, ToSocketAddrs, UdpSocket},
+    time::Duration, collections::HashMap, sync::{Arc, MutexGuard, Mutex, mpsc}, thread, borrow::BorrowMut,
+};
 
-use anyhow::{Context, bail};
+use anyhow::{bail, Context};
 use byteorder::{BigEndian, ByteOrder};
-use rand::random;
+use popol::{interest, Event};
+use rand::{random, Rng};
 
-
+#[derive(Eq, PartialEq, Clone, Hash, Debug)]
+enum SourceKey {
+    Peer(SocketAddr),
+}
 
 pub struct Magdl;
 impl Magdl {
@@ -18,26 +25,103 @@ impl Magdl {
         let magnet = Magnet::from_link_string(magnet_link);
 
         // Initialize trackers and construct list of available peers
-        let tracker_connections: Vec<TrackerConnection> = magnet.tracker_urls.into_iter().filter_map(|url| {
-            match TrackerConnection::establish(url) {
+        let tracker_connections: Vec<TrackerConnection> = magnet
+            .tracker_urls
+            .into_iter()
+            .filter_map(|url| match TrackerConnection::establish(url) {
                 Ok(conn) => {
                     println!("Connected to: {}", conn.url);
                     Some(conn)
-                },
-                Err(e) => { println!("{}", e); None },
-            }
-        }).collect();
+                }
+                Err(e) => {
+                    println!("{}", e);
+                    None
+                }
+            })
+            .collect();
+
+        let mut peer_id = [0u8; 20];
+        rand::thread_rng().fill(&mut peer_id[..]);
+        let signature = "-WM0001-";
+        peer_id[0..signature.len()].copy_from_slice(signature.as_bytes());
+
+        let peer_addrs: Vec<SocketAddr> = tracker_connections
+            .iter()
+            .filter_map(|tracker| {
+                let announce_rd = AnnounceRequestDescriptor {
+                    connection_id: tracker.connection_id,
+                    peer_id: peer_id.clone(),
+                    info_hash: magnet.info_hash.clone(),
+                    downloaded: 0,
+                    left: 0,
+                    uploaded: 0,
+                    event: AnnounceEvent::None,
+                };
+                match tracker.list_peer_addrs(announce_rd) {
+                    Ok(peers) => Some(peers),
+                    Err(_) => {
+                        println!("Failed to retrieve peer list from tracker");
+                        None
+                    }
+                }
+            })
+            .flatten()
+            .collect();
+        dbg!("Retrieved {} peer addresses", peer_addrs.len());
 
         // Connect to as many peers as possible and perform handshake
-        todo!();
+        let sources = Arc::new(Mutex::new(popol::Sources::new()));
+        let events = Arc::new(Mutex::new(Vec::<Event<SourceKey>>::new()));
+        let source_map = Arc::new(Mutex::new(HashMap::new()));
+
+        let p_sources = sources.clone();
+        let p_source_map = source_map.clone();
+        let peer_thread = thread::spawn(move || {
+            peer_addrs
+                .into_iter()
+                .for_each(|addr| {
+                    match PeerConnection::establish(addr) {
+                        Ok(conn) => {
+                            let key = SourceKey::Peer(conn.addr);
+                            p_sources.lock().unwrap().register(key.clone(), &conn.socket, interest::ALL);
+                            p_source_map.lock().unwrap().insert(key.clone(), conn);
+                        },
+                        Err(e) => println!("Failed to connect to peer {}", e)
+
+                    }
+                })
+        });
 
         // Main execution loop
         loop {
+            {
+                let events: &mut Vec<_> = &mut events.lock().unwrap();
+                sources.lock().unwrap().wait(events)?;
+            }
+
+            for event in events.lock().unwrap().drain(..) {
+                let key = event.key;
+                let source = event.source;
+                let peer_conn = source_map.lock().unwrap().get(&key).expect("Registered source has no peer counterpart");
+                if source.is_error() || source.is_invalid() || source.is_hangup() {
+                    sources.lock().unwrap().unregister(&key);
+                    dbg!("Disconnected or errored", key);
+                }
+                if event.source.is_readable() {
+                    dbg!("Peer is readable");
+
+                }
+                if event.source.is_writable() {
+                    dbg!("Peer is writable");
+
+                }
+            }
+
             // Receive messages from peers and update their state
-            todo!();
+            // todo!();
 
             // Request pieces from peers
-            todo!();
+            // todo!();
         }
     }
 }
@@ -61,7 +145,8 @@ impl Magnet {
             match id {
                 "xt" => {
                     let info_string = value[value.len() - 40..].as_bytes();
-                    let bytes = hex::decode(info_string).expect("Failed to parse info hash from magnet link");
+                    let bytes = hex::decode(info_string)
+                        .expect("Failed to parse info hash from magnet link");
                     exact_topic.copy_from_slice(bytes.as_slice());
                 }
                 "dn" => {
@@ -124,7 +209,141 @@ impl TrackerConnection {
             url,
         })
     }
+    pub fn list_peer_addrs(
+        &self,
+        request_descriptor: AnnounceRequestDescriptor,
+    ) -> anyhow::Result<Vec<SocketAddr>> {
+        let request = AnnounceRequest::new(request_descriptor);
+        self.socket.send(request.to_bytes().as_slice())?;
+        let mut response_buf = [0u8; 4092];
+        let bytes_read = self.socket.recv(&mut response_buf)?;
+        if bytes_read == 4092 {
+            println!("May have overrun announce response buffer");
+        }
+        let response = AnnounceResponse::from_bytes(&response_buf, bytes_read);
+        Ok(response.peers)
+    }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum AnnounceEvent {
+    None = 0,
+    Completed,
+    Started,
+    Stopped,
+}
 
+#[derive(Debug)]
+struct AnnounceRequest {
+    connection_id: i64,
+    action: u32,
+    transaction_id: u32,
+    info_hash: [u8; 20],
+    peer_id: [u8; 20],
+    downloaded: u64,
+    left: u64,
+    uploaded: u64,
+    event: AnnounceEvent,
+    ip_address: u32,
+    key: u32,
+    num_want: i32,
+    port: u16,
+}
 
+#[derive(Debug)]
+pub struct AnnounceRequestDescriptor {
+    pub connection_id: i64,
+    pub peer_id: [u8; 20],
+    pub info_hash: [u8; 20],
+    pub downloaded: u64,
+    pub left: u64,
+    pub uploaded: u64,
+    pub event: AnnounceEvent,
+}
+
+const ANNOUNCE_REQUEST_BYTES: usize = 98;
+impl AnnounceRequest {
+    fn new(descriptor: AnnounceRequestDescriptor) -> Self {
+        Self {
+            connection_id: descriptor.connection_id,
+            action: 1,
+            transaction_id: rand::random(),
+            info_hash: descriptor.info_hash,
+            peer_id: descriptor.peer_id,
+            downloaded: descriptor.downloaded,
+            left: descriptor.left,
+            uploaded: descriptor.uploaded,
+            event: descriptor.event,
+            ip_address: 0,
+            key: rand::random(),
+            num_want: -1,
+            port: 6881,
+        }
+    }
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![0u8; ANNOUNCE_REQUEST_BYTES];
+        BigEndian::write_i64(&mut bytes[0..8], self.connection_id);
+        BigEndian::write_u32(&mut bytes[8..12], self.action);
+        BigEndian::write_u32(&mut bytes[12..16], self.transaction_id);
+        bytes[16..36].copy_from_slice(&self.info_hash);
+        bytes[36..56].copy_from_slice(&self.peer_id);
+        BigEndian::write_u64(&mut bytes[56..64], self.downloaded);
+        BigEndian::write_u64(&mut bytes[64..72], self.left);
+        BigEndian::write_u64(&mut bytes[72..80], self.uploaded);
+        BigEndian::write_u32(&mut bytes[80..84], self.event as u32);
+        BigEndian::write_u32(&mut bytes[84..88], self.ip_address);
+        BigEndian::write_u32(&mut bytes[88..92], self.key);
+        BigEndian::write_i32(&mut bytes[92..96], self.num_want);
+        BigEndian::write_u16(&mut bytes[96..98], self.port);
+        bytes
+    }
+}
+
+#[derive(Debug)]
+struct AnnounceResponse {
+    action: u32,
+    transaction_id: u32,
+    interval: u32,
+    leechers: u32,
+    seeders: u32,
+    peers: Vec<SocketAddr>,
+}
+impl AnnounceResponse {
+    fn from_bytes(bytes: &[u8], length: usize) -> Self {
+        let action = BigEndian::read_u32(&bytes[0..4]);
+        let transaction_id = BigEndian::read_u32(&bytes[4..8]);
+        let interval = BigEndian::read_u32(&bytes[8..12]);
+        let leechers = BigEndian::read_u32(&bytes[12..16]);
+        let seeders = BigEndian::read_u32(&bytes[16..20]);
+        let peer_list = &bytes[20..length];
+        if peer_list.len() % 6 != 0 {
+            panic!("Invalid peer list size");
+        }
+        let mut peers = Vec::new();
+        for address in peer_list.chunks(6) {
+            let ip = Ipv4Addr::new(address[0], address[1], address[2], address[3]);
+            let port = BigEndian::read_u16(&address[4..6]);
+            let peer = SocketAddr::new(IpAddr::V4(ip), port);
+            peers.push(peer);
+        }
+        Self {
+            action,
+            transaction_id,
+            interval,
+            leechers,
+            seeders,
+            peers,
+        }
+    }
+}
+
+struct PeerConnection {
+    socket: TcpStream,
+    addr: SocketAddr,
+}
+impl PeerConnection {
+    pub fn establish(addr: SocketAddr) -> anyhow::Result<Self> {
+        let socket = TcpStream::connect_timeout(&addr, Duration::from_secs(1))?;
+        Ok(Self { socket, addr })
+    }
+}
